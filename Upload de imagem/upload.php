59b4c9b8af7b70fea9356a7f2495726b337ba168
Lucas de Almeida <54106868+lucasdealmeidadev@users.noreply.github.com>
2020-03-11 21:58:17 -0300

    Upload de imagem

---
+++ Upload de imagem/upload.php
@@ -0,0 +1,45 @@
+<?php
+
+    function validate(array $file): string
+    {
+        $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $extension = strtolower($extension);                                                                                                                                                           
+ 
+        if(!empty($file) && $file['error'] != 0){
+            return 'Erro : Não foi possível realizar o upload!';
+        }
+
+        if($file['size'] == 0){
+            return 'Erro : Tamanho do arquivo é igual a zero!';
+        }
+
+        if($file['size'] > 100000){
+            return 'Erro : Tamanho maior que o permitido (100 kbytes)';
+        }
+
+        if(!strstr( '.jpg;.jpeg;.pjpeg;.gif;.png;.bmp', $extension)){
+           return 'Erro : Formato do arquivo é inválido!';
+        }
+        return '';
+    }
+
+    function upload(array $file): string
+    {
+        $dir  = 'imgs/' . $file['name'];
+    
+        if(move_uploaded_file($file['tmp_name'], $dir)){
+            return "<img width='120' heigth='150' src='imgs/" . $file['name'] . "'>";
+        }
+        else{
+            return 'Erro : Não foi possível enviar o arquivo para o destino!';
+        }
+    }
+
+    $file = (array) $_FILES['file'];
+
+    if(!empty($message = validate($file))){
+        echo $message;
+    }
+    else{
+        echo upload($file);
+    }
